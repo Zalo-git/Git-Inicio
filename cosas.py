30bd27a143688dbbb9a39a51efbaf7f5902c5c80
gdiaz <zalodiazg@gmail.com>
2026-08-15 06:10:43 -0400

    Cambio en cosas.py linea 4 y 5 en main despues de bifurcar en login

--- cosas.py
+++ cosas.py
@@ -1,2 +1,5 @@
 print("Me cambiaron, esto va pal tercer git, no se si me van a aceptar el cambio, pero bueno, vamos a ver que pasa")
 print("What? pegue una mierda")
+
+print("Hola Gonza, este es el momento en donde creamos un commit en main")
+print("Precisamente despues de crear un commit en login, veamos que pasa")
