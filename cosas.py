63c8192f103ed827a445397870e56c2f9ddf7e3e
gdiaz <zalodiazg@gmail.com>
2026-08-15 02:42:23 -0400

    Este es mi tercer commit no me lo puedo creer, un dia despues como veras en las fechas, solo cambie una cosa en el cosas.py y trabaje un poco con checkout y reset

--- cosas.py
+++ cosas.py
@@ -1,1 +1,2 @@
-print("Wena po, empecemos con git")
+print("Me cambiaron, esto va pal tercer git, no se si me van a aceptar el cambio, pero bueno, vamos a ver que pasa")
+print("What? pegue una mierda")
