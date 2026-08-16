051dbf00e9a2caf557e565138f10da21a80a4e3c
gonzalodiazi-cpu <gonzalo.diaz.i@ug.uchile.cl>
2026-08-16 04:35:24 -0400

    Gilipolles en cosas.py linea 2

--- cosas.py
+++ cosas.py
@@ -1,5 +1,5 @@
 print("Bienvenido a github, obligao a cambiar esta primera linea, 3 horas de video")
-print("What? pegue una mierda")
+print("FUNCIONOOOOCTMMMMMMMMVAMOOOOOOOOOOOSSS")
 
 print("Hola Gonza, este es el momento en donde creamos un commit en main")
 print("Precisamente despues de crear un commit en login, veamos que pasa")
